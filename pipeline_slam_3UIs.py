cf24f4d55936e01297b932cb2f078610a66eb781
Neil Shephard <n.shephard@sheffield.ac.uk>
2024-11-14 17:17:27 +0000

    refactor: Switching to f-strings and removing CamelCase
    - `featureCountsReadAssignments` > `feature_count_read_assignments` as per
      [PEP8](https://peps.python.org/pep-0008/#function-and-variable-names)
    - Switches to [f-strings](https://realpython.com/python-f-strings/) for interpolating filenames
    - Splits long lines into strings that are concatenated such that they are easy to read in the code and in the log (as
      this removes long white space between options that was used to align text in the code)

--- pipeline_slam_3UIs.py
+++ pipeline_slam_3UIs.py
@@ -23,15 +23,15 @@ PARAMS = P.get_parameters(
 
 @ruffus.follows(ruffus.mkdir("name_sorted_bams"))
 @ruffus.transform(
-    "input_bams.dir/*.bam",
-    ruffus.regex("input_bams.dir/(.+).bam"),
+    "bam/*.bam",
+    ruffus.regex("bam/(.+).bam"),
     r"name_sorted_bams/\1.sorted.bam",
 )
 def name_sort_bams(infile, outfile):
     """
     Takes `.bam` file and sorts it in name order, therefore reads from the same pair appear sequentially
     """
-    statement = "samtools sort -n %(infile)s -o %(outfile)s"
+    statement = f"samtools sort -n {infile} -o {outfile}"
     P.run(statement, job_memory="8G")
 
 
@@ -41,21 +41,27 @@ def name_sort_bams(infile, outfile):
     ruffus.regex("name_sorted_bams/(.+).sorted.bam"),
     r"read_assignments/\1/\1.sorted.assigned.bam",
 )
-def featureCountsReadAssignments(infile, outfile):
+def feature_count_read_assignments(infile, outfile):
     """
     Takes `.bam` file and adds 2 tags to it:
         - XT tag tells us the transcript the reads map to
         - XS tag tells us which strand the gene is transcribed from
+
+    NB The `-p` flag is required for SubRead v2.0.6, may need to make this flexible.
     """
     annotation = PARAMS["transcript_gtf"]
     outdir = os.path.dirname(outfile)
     rename_outfile = outfile.replace(".assigned.bam", "")
-    statement = """featureCounts -a %(annotation)s
-                                 -o %(outdir)s/counts.txt
-                                 -T 2
-                                 -R BAM
-                                 %(infile)s &&
-                    mv %(rename_outfile)s.bam.featureCounts.bam %(outfile)s"""
+    statement = (
+        "featureCounts"
+        "-p "
+        f"-a {annotation} "
+        f"-o {outdir}/counts.txt "
+        "-T 2 "
+        "-R BAM"
+        f"{infile} &&"
+        "mv %(rename_outfile)s.bam.featureCounts.bam %(outfile)s"
+    )
     P.run(statement, job_threads=2, job_memory="8G")
 
 
@@ -73,11 +79,12 @@ def VCF_from_no4sU_control(infile, outfile):
     """
     outfile_uncompressed = outfile.replace(".vcf.gz", ".vcf")
     genome_fasta = PARAMS["genome_fasta"]
-    statement = """samtools mpileup -B -A -f %(genome_fasta)s %(infile)s |
-                    varscan mpileup2snp --variants 1 --output-vcf 1 > %(outfile_uncompressed)s &&
-                    bcftools view %(outfile_uncompressed)s -Oz -o %(outfile)s"""
-    # removed sort -k1,1 -k2,2n %(outfile_uncompressed)s -o %(outfile_uncompressed)s &&
-    # before bcftools view
+    statement = (
+        f"samtools mpileup -B -A -f {genome_fasta} {infile} | "
+        f"varscan mpileup2snp --variants 1 --output-vcf 1 > {outfile_uncompressed} && "
+        # f"sort -k1,1 -k2,2n {outfile_uncompressed} -o {outfile_uncompressed} &&"
+        f"bcftools view {outfile_uncompressed} -Oz -o {outfile}"
+    )
     P.run(statement, job_threads=12, job_memory="1G")
 
 
@@ -86,13 +93,13 @@ def index_VCF(infile, outfile):
     """
     Indexes the VCF file so it can be read with Pysam, generates the .vcf.gz.tbi file
     """
-    statement = "tabix -p vcf %(infile)s"
+    statement = f"tabix -p vcf {infile}"
     P.run(statement)
 
 
 @ruffus.follows(index_VCF, ruffus.mkdir("all_introns_counts_and_info"))
 @ruffus.transform(
-    featureCountsReadAssignments,
+    feature_count_read_assignments,
     ruffus.regex("read_assignments/(.+)/(.+)_(.+)_(.+)_(.+)_(.+).sorted.assigned.bam"),
     ruffus.add_inputs(r"snp_vcf/\2.vcf.gz"),
     r"all_introns_counts_and_info/\1.tsv",
@@ -103,7 +110,7 @@ def all_introns_counts_and_info(infiles, outfile):
     python script that uses pysam (python wrapper for htslib). This script iterates over the `.bam`
     file pair-by-pair (representing the sequencing insert), determines whether the read-pair shows
     evidence of intron splicing/retention and assigns these to specific events by referencing the
-    `.gtf` and `.bed` files, and XT tag from featureCountsReadAssignments. Next, the script uses the
+    `.gtf` and `.bed` files, and XT tag from feature_count_read_assignments. Next, the script uses the
     XS tag from featureCountsReadAssignment to assign each read in the pair as the forward or reverse
     read, relative to the direction of transcription. Finally, it looks for T>C in FW read, A>G in RV
     read, checks these are not present in the SNP VCF file, and outputs metadata on each read-pair
@@ -117,12 +124,13 @@ def all_introns_counts_and_info(infiles, outfile):
         + "/pipeline_slam_3UIs/all_introns_counts_and_info.py"
     )
 
-    statement = """python %(script_path)s -b %(bamfile)s
-                                            -g %(annotation)s
-                                            -o %(outfile)s
-                                            -vcf %(vcffile)s
-                                            -bed %(utron_bed)s -v5"""
-
+    statement = (
+        f"python {script_path} -b {bamfile} "
+        f"-g {annotation} "
+        f"-o {outfile} "
+        f"-vcf {vcffile} "
+        f"-bed {utron_bed} -v5"
+    )
     P.run(statement, job_memory="16G")
 
 
@@ -143,14 +151,14 @@ def summarize_all_introns_counts(infiles, outfile):
         os.path.dirname(os.path.abspath(__file__))
         + "/pipeline_slam_3UIs/summarize_counts.R"
     )
-    statement = "Rscript %(script_path)s %(input_folder)s %(day_regex)s %(outfile)s"
+    statement = f"Rscript {script_path} {input_folder} {day_regex} {outfile}"
 
     P.run(statement, job_memory="64G")
 
 
 @ruffus.follows(
     name_sort_bams,
-    featureCountsReadAssignments,
+    feature_count_read_assignments,
     VCF_from_no4sU_control,
     index_VCF,
     all_introns_counts_and_info,
